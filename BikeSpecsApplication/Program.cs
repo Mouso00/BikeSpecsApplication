ae880e44b4047653563ec355dfad0ad0dfa4c4d8
Mouso00@DESKTOP <Mousahmed00@gmail.com>
2022-12-23 18:25:08 +0200

    The second commit of the project 2 functionality works good

--- BikeSpecsApplication/Program.cs
+++ BikeSpecsApplication/Program.cs
@@ -23,6 +23,7 @@ namespace StringManipulation
     {
         static void Main(string[] args)
         {
+           
             bool showMenu = true;
             while (showMenu)
             {
@@ -44,7 +45,7 @@ namespace StringManipulation
                     CaptureInput();
                     return true;
                 case "2":
-                   
+                    ShowList();
                     return true;
                 case "3":
                     return false;
@@ -53,11 +54,15 @@ namespace StringManipulation
             }
         }
 
-        private static string CaptureInput()
+        public static void CaptureInput()
         {
+            List<String> userList = new List<String>();
+
+
             Console.Clear();
             Console.Write("Enter the Brand of the bike : ");
-            Console.ReadLine();
+            var input = Console.ReadLine(); 
+            userList.Add(input);
             Console.Write("Enter the Model of the bike : ");
             Console.ReadLine();
             Console.Write("Enter the Age of the bike : ");
@@ -65,8 +70,32 @@ namespace StringManipulation
             Console.Write("Enter the Category of the bike : ");
             Console.ReadLine();
             Console.Write("Enter the CC of the bike : ");
-            return Console.ReadLine();
+            Console.ReadLine() ;
+
+            
+      /*  public static string StoreInput()
+        {
+            String input = Console.ReadLine();
+            List<String> userList = new List<String>();
+            userList.Add(input);
+
+      
+            foreach (var user in userList)
+            {
+                Console.WriteLine(user);
+            }*/
+
         }
-       
+
+        public static void ShowList()
+        {
+           
+            Console.Clear();
+
+            Console.WriteLine("This is your previous Entry" );
+            Console.ReadKey();
+
+        }
+
     }
 }
